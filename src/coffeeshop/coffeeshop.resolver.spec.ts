4618f10e0aac3bef6fe38d1d8f76fa18c7b504f6
Nagasai Ghanta <Nagasai_Ghanta@epam.com>
2020-11-26 13:21:47 +0530

    unit testing for calculate order price mutation

--- src/coffeeshop/coffeeshop.resolver.spec.ts
+++ src/coffeeshop/coffeeshop.resolver.spec.ts
@@ -1,10 +1,10 @@
-import { CoffeeShopController } from './coffeshop.controller';
 import {CoffeeShopResolver} from './coffeshop.resolver';
 import {CoffeeShopService} from './coffeshop.service';
-import {list} from './mock_productList';
+import {list, cartData} from './mock_productList';
 import {discountObj} from './discountListMock';
-import { CoffeeShopModule } from './coffeeshop.module';
+
 describe("test coffeeshop resolver", () => {
+
     describe("test get products", () => {
         it("get call for products", async () => {
             const service = new CoffeeShopService();
@@ -14,6 +14,7 @@ describe("test coffeeshop resolver", () => {
             expect(service.getProductsList).toHaveBeenCalled();
         })
     })
+
     describe("test get discounts", () => {
         it("get call for discounts", async () => {
             const service = new CoffeeShopService();
@@ -22,6 +23,24 @@ describe("test coffeeshop resolver", () => {
             const response = await resolver.getDiscountList();
             expect(service.getDiscountList).toHaveBeenCalled();
             expect(response).toEqual(discountObj);
+        })
+    })
+
+    const finalResponse = {
+        finalPrice: 151,
+        discountAmount: 8.35,
+        totalTax: 7.13,
+        totalPaymentRequired: 149.78
+    }
+    
+    describe("test order summary calculation", () => {
+        it("order summary", async () => {
+            const service = new CoffeeShopService();
+            service.calculateOrderPrice = jest.fn().mockReturnValue(finalResponse);
+            const resolver = new CoffeeShopResolver(service);
+            const response = await resolver.calculateOrderPrice(cartData);
+            expect(service.calculateOrderPrice).toHaveBeenCalled();
+            expect(response).toEqual(finalResponse);
         })
     })
 })
--- src/coffeeshop/mock_productList.ts
+++ src/coffeeshop/mock_productList.ts
@@ -1,3 +1,26 @@
+export const cartData = {
+    orderSummary: [{
+        type: "bevarage",
+        items: [{
+            id: 1,
+            price: 4,
+            quantity: 4,
+            taxCategory: "level_2"
+        }]
+    },
+    {
+        type: "food",
+        items: [{
+            id: 5,
+            price: 45,
+            quantity: 3,
+            taxCategory: "level_2"
+        }]
+    }
+    ],
+    total: 7,
+    discountIds: [2, 3]
+}
 export const list = {
     "data": [
         {
